e755caee148ea6aafb6fb9642aa57c1efd37a7ce
Oumaima Azzat <oumaimazzat@gmail.com>
2020-09-14 16:33:45 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,8 @@
 def position_taken?(board = [" "," "," "," "," "," "," "," "," "], index = "X")
   taken = nil
   if (board[index] ==  " " || board[index] == "" || board[index] == nil)
-  #   taken = false
-  # else
-  #   taken = true
-end
+     taken = false
+  else
+     taken = true
+  end
+end 
